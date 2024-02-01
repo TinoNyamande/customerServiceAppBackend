b7fde44cdf885d0879e99c047402eed939907585
Tinotenda Nyamande <tnyamande@IMGSOL.co.zw>
2024-02-01 16:17:24 +0200

    Admin Panel controller actions

---
+++ EmailCustomerServiceApi/Migrations/20240131140935_addFields.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmailCustomerServiceApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FromName",
+                table: "Emails",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TimeAssigned",
+                table: "Emails",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TimeResolved",
+                table: "Emails",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TimeTaken",
+                table: "Emails",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FromName",
+                table: "Emails");
+
+            migrationBuilder.DropColumn(
+                name: "TimeAssigned",
+                table: "Emails");
+
+            migrationBuilder.DropColumn(
+                name: "TimeResolved",
+                table: "Emails");
+
+            migrationBuilder.DropColumn(
+                name: "TimeTaken",
+                table: "Emails");
+        }
+    }
+}
